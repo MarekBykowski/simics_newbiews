bc191002ed67266ebb727f94d7ed519384c00ffc
Marek Bykowski <marek.bykowski@gmail.com>
2023-01-29 09:05:33 +0000

    init commit

---
+++ modules/plugin_device/test/s-plugin_device.py
@@ -0,0 +1,9 @@
+import dev_util
+import conf
+import stest
+import plugin_device_common
+
+# Create an instance of the device to test
+dev = plugin_device_common.create_plugin_device()
+
+# Write your tests here
