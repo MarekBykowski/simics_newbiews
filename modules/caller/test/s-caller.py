83e4f3cbb2ead51b9f1d92d960811ae99bdc445f
Marek Bykowski <marek.bykowski@gmail.com>
2023-02-04 15:56:51 +0000

    init commit

---
+++ modules/caller/test/s-caller.py
@@ -0,0 +1,11 @@
+# © 2010 Intel Corporation
+
+import dev_util
+import conf
+import stest
+import caller_common
+
+# Create an instance of the device to test
+dev = caller_common.create_caller()
+
+# Write your tests here
--- modules/simple_device/test/s-simple_device.py
+++
@@ -1,9 +0,0 @@
-import dev_util
-import conf
-import stest
-import simple_device_common
-
-# Create an instance of the device to test
-dev = simple_device_common.create_simple_device()
-
-# Write your tests here
